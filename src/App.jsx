6c207ae8cd8a80c29b6cbe8887fa0e7c3294003e
travistn <travisn714@gmail.com>
2023-01-30 13:57:32 -0800

    Added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import Home from './pages/Home';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 };
 
 export default App;
