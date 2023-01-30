cf7a3492aa1926a85a9451040dda9342c403a9cc
travistn <travisn714@gmail.com>
2023-01-30 15:43:32 -0800

    Menu appears when clicking on menu icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import logo from '../assets/logo.svg';
 import menuIcon from '../assets/icon-hamburger.svg';
+import closeIcon from '../assets/icon-close.svg';
 
 const links = ['Pricing', 'Product', 'About Us', 'Careers', 'Community'];
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className='w-[90%] m-auto'>
       <div className='flex flex-row pt-8 px-4 lg:px-8 items-center'>
@@ -13,10 +16,16 @@ const Navbar = () => {
           src={logo}
           className='w-[110px] h-[18px] lg:w-[147px] lg:h-[24px] hover:cursor-pointer hover:opacity-80'
         />
-        <img src={menuIcon} className='w-[25px] h-[18px] ml-auto lg:hidden' />
+        <img
+          src={!toggleMenu ? menuIcon : closeIcon}
+          className='w-[20px] h-[18px] ml-auto lg:hidden'
+          onClick={() => setToggleMenu(!toggleMenu)}
+        />
         <ul className='hidden lg:flex flex-row gap-8 items-center m-auto mr-0'>
-          {links?.map((link) => (
-            <li className='text-[13px] font-medium leading-[19px] text-dark-blue hover:cursor-pointer hover:opacity-50'>
+          {links?.map((link, index) => (
+            <li
+              key={index}
+              className='text-[13px] font-medium leading-[19px] text-dark-blue hover:cursor-pointer hover:opacity-50'>
               {link}
             </li>
           ))}
@@ -25,6 +34,17 @@ const Navbar = () => {
           Get Started
         </button>
       </div>
+      {toggleMenu && (
+        <div className='lg:hidden w-[90%] h-[290px] m-auto mt-4 bg-gray-50 rounded-md'>
+          <ul className='flex flex-col gap-6 items-center justify-center h-full'>
+            {links?.map((link, index) => (
+              <li key={index} className='text-[16px] font-bold leading-[23px] text-dark-blue'>
+                {link}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
