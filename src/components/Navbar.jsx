9d82f3927800412734858fb9e6db965be09330e5
travistn <travisn714@gmail.com>
2023-01-30 14:12:11 -0800

    Added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import logo from '../assets/logo.svg';
+import menuIcon from '../assets/icon-hamburger.svg';
 
 const Navbar = () => {
   return (
-    <div className='flex flex-row px-4 pt-8'>
-      <img src={logo} className='w-[110px] h-[18px]' />
+    <div className='w-[11/12]'>
+      <div className='flex flex-row pt-8 px-8'>
+        <img src={logo} className='w-[110px] h-[18px]' />
+        <img src={menuIcon} className='w-[25px] h-[18px] ml-auto lg:hidden' />
+      </div>
     </div>
   );
 };
